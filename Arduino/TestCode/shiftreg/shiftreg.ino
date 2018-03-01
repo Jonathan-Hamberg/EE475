ccf1a32582d455bf746ffafa27d9bb3e215c2b04
daniel predmore <git@danielpredmore.com>
2018-02-28 22:11:36 -0800

    Added a button class (untested)

--- Arduino/TestCode/shiftreg/shiftreg.ino
+++ Arduino/TestCode/shiftreg/shiftreg.ino
@@ -1,32 +1,36 @@
 #define GREEN_PIN 2
-#define DATA_PIN 3
+#define BLUE_PIN 3
+#define RED_PIN 6
 #define RCLK_PIN 4
 #define SRCLK_PIN 5
+#define REG_CLOCK 5
+#define DATA_PIN 7
+#define LOAD_PIN 8
 
-#include "ShiftReg.h"
+#include <ShiftOut.h>
+#include <ShiftIn.h>
+#include <RGB_LED.h>
+#include <Button.h>
 
-byte count;
-byte count1;
-static unsigned int pins[] = {DATA_PIN, GREEN_PIN};
-ShiftReg reg;
+static unsigned int pins[] = {BLUE_PIN, GREEN_PIN, RED_PIN};
+ShiftOut out;
+ShiftIn in(REG_CLOCK, LOAD_PIN, DATA_PIN, 1);
+RGB_LED led(&out, 0, 16, 0, 8, 0, 0);
+Button button(0, &in);
 
 
 void setup() {
   //Serial.begin(9600);
-  reg.build(1, 2, pins, RCLK_PIN, SRCLK_PIN);
-  count = 0;
-  count1 = 1;
-
+  out.build(3, 1, pins, RCLK_PIN, SRCLK_PIN);
+  pinMode(REG_CLOCK, OUTPUT);
+  pinMode(DATA_PIN, INPUT);
+  pinMode(LOAD_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  reg.setReg(0, 0, count);
-  reg.setReg(0, 1, count1);
-  
-  reg.load();
-  count++;
-  count1 = ((~count1) << 7) | (count1 >> 1);
-
-  delay(250);
+  byte value = in.getReg(1);
+  out.setReg(0,0,value);
+  out.load();
+  in.load();
 }
